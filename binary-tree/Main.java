74d6875c418d246afdb111d5bd28d4df1ff55102
Erick λ <96489518+erickcestari@users.noreply.github.com>
2024-04-10 19:18:44 -0300

    refactor: remove some comments

--- binary-tree/Main.java
+++ binary-tree/Main.java
@@ -1,18 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    // BinaryTree bTree = new BinaryTree();
-    // bTree.setRoot(new Node(10));
-    // var left = bTree.addLeft(bTree.getRoot(), -1);
-    // var right = bTree.addRight(bTree.getRoot(), 1);
-
-    // bTree.addRight(left, 2);
-    // bTree.addLeft(left, -2);
-    // bTree.addRight(right, 2);
-    // bTree.addLeft(right, -2);
-
-    
-    // bTree.printTreePreOrder();
-
     // var vBTree = new VBinaryTree(3);
     // vBTree.loadTree();
     // vBTree.pre_ordem(0);
