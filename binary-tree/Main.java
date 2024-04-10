9d3ff4560057938ca7b25957a8cffe65dfe5facc
Erick λ <96489518+erickcestari@users.noreply.github.com>
2024-04-10 19:17:03 -0300

    feat: add binary tree

--- binary-tree/Main.java
+++ binary-tree/Main.java
@@ -1,1 +1,28 @@
+public class Main {
+  public static void main(String[] args) {
+    // BinaryTree bTree = new BinaryTree();
+    // bTree.setRoot(new Node(10));
+    // var left = bTree.addLeft(bTree.getRoot(), -1);
+    // var right = bTree.addRight(bTree.getRoot(), 1);
 
+    // bTree.addRight(left, 2);
+    // bTree.addLeft(left, -2);
+    // bTree.addRight(right, 2);
+    // bTree.addLeft(right, -2);
+
+    
+    // bTree.printTreePreOrder();
+
+    // var vBTree = new VBinaryTree(3);
+    // vBTree.loadTree();
+    // vBTree.pre_ordem(0);
+    // System.out.println();
+    // vBTree.in_ordem(0);
+    // System.out.println();
+    // vBTree.pos_ordem(0);
+
+    var arvore = new DynamicBinaryTree();
+    arvore.buildTree(null, 's', 0);
+    arvore.printPreOrderLeft(arvore.getRoot());
+  }
+}
